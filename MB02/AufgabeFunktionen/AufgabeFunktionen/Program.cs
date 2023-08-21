fc70429d9420b100e201ef1294a0c31669209143
Vivienne Gschwend <viviennegschwend@gmail.com>
2023-08-21 15:50:08 +0200

    Add exercise ggT / KgV 1-3

---
+++ MB02/AufgabeFunktionen/AufgabeFunktionen/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace AufgabeFunktionen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 12, b = 16;
+            Console.WriteLine(MyMath.Calc_ggT(a, b));
+            Console.WriteLine(MyMath.Calc_kgV(a, b));
+        }
+    }
+}
