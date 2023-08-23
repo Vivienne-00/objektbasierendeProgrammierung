c739ddb5b9df634a882dd1acce18346bc1dd5af7
Vivienne Gschwend <viviennegschwend@gmail.com>
2023-08-23 23:20:56 +0200

    Add exercise Aggregation

---
+++ MB04/AufgabeAggregation/FirmaUndMitarbeiter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AufgabeAggregation
+{
+    internal class FirmaUndMitarbeiter
+    {
+        public int MitarbeiterId { get; set; }
+        public string Name { get; set; }
+        public Firma Firma { get; set; }
+    }
+}
