8dea4759a6150d9022bd6aa688597d41cfbf3455
Vivienne Gschwend <viviennegschwend@gmail.com>
2023-08-23 23:21:20 +0200

    Add exercise Komposition

---
+++ MB04/AufgabeKomposition/Konto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AufgabeKomposition
+{
+    internal class Konto
+    {
+        public int KontoId { get; set; }
+        public string Name { get; set; }
+        public Kunde Kunde { get; set; }
+    }
+}
---
+++ MB04/AufgabeKomposition/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AufgabeKomposition
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+        }
+    }
+}
