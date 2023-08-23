bfd42f20dadb8bd8cf1d9da6b3571f4b4ed0c973
Vivienne Gschwend <viviennegschwend@gmail.com>
2023-08-23 22:47:20 +0200

    Add exercise Kapselung

---
+++ MB04/AufgabeKapselung/AufgabeKapselung/Person.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AufgabeKapselung
+{
+    internal class Person
+    {
+        private int wutLevel = 0;
+        private int appetitLevel = 0;
+        public void Provozieren(int level)
+        {
+            wutLevel += level;
+
+            if (wutLevel >= 100)
+            {
+                Ausrasten();
+            }
+            else
+            {
+                Console.WriteLine($"Person ist auf dem Wutlevel {wutLevel}");
+            }
+        }
+        private void Ausrasten()
+        {
+            Console.WriteLine($"Person ist am ausrasten und auf dem Wutlevel {wutLevel}");
+        }
+
+        public void EssenGeben(string antwort)
+        {
+            if ( antwort == "ja") 
+            {
+                wutLevel = 0;
+                appetitLevel = 0;
+                Console.WriteLine($"Die Person konnte sich beruhigen und ist auf dem Wutlevel {wutLevel}");
+            } 
+            else
+            {
+                EssenVerweigern();
+            }
+        }
+
+        public void EssenVerweigern()
+        {
+            wutLevel = 15 + wutLevel;
+            appetitLevel = 15 + appetitLevel;
+            Console.WriteLine($"Die Person ist hangry und auf dem Wutlevel {wutLevel}");
+        }
+
+        public void ArbeitsauftragGeben(int level)
+        {
+            if (level != 0)
+            {
+                wutLevel = level + wutLevel;
+                appetitLevel = level + appetitLevel;
+                Console.WriteLine($"Die Person ist von der Arbeit erschöpft und auf dem Wutlevel {wutLevel}");
+            } else
+            {
+                Console.WriteLine($"Der Zustand der Person bleibt gleich, Wutlevel {wutLevel}");
+            }
+            
+        }
+    }
+}
---
+++ MB04/AufgabeKapselung/AufgabeKapselung/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace AufgabeKapselung
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int antwortProvokation = ProvokationsLevel();
+            string antwortEssen = HungerLevel();
+            int antwortArbeit = ArbeitsLevel();
+            Person person = new Person();
+            person.Provozieren(antwortProvokation);
+            person.EssenGeben(antwortEssen);
+            person.ArbeitsauftragGeben(antwortArbeit);
+        }
+        public static int ProvokationsLevel()
+        {
+            int antwortProvokation;
+            do
+            {
+                Console.Write("Werden Sie provoziert von 1-100? ");
+                string antwort = Console.ReadLine();
+                int.TryParse(antwort, out antwortProvokation);
+            } 
+            while (antwortProvokation <= 0);
+            return antwortProvokation;
+        }
+        public static string HungerLevel()
+        {
+            string antwortEssen;
+            do
+            {
+                Console.Write("Haben Sie etwas gegessen, ja oder nein? ");
+                antwortEssen = Console.ReadLine();
+
+            }
+            while (antwortEssen.Length < 0);
+            return antwortEssen;
+        }
+        public static int ArbeitsLevel()
+        {
+            int antwortArbeit;
+            do
+            {
+                Console.Write("Wie viel arbeiten Sie von 1-100? ");
+                string antwort = Console.ReadLine();
+                int.TryParse(antwort, out antwortArbeit);
+            } 
+            while (antwortArbeit <= 0);
+            return antwortArbeit;
+        }
+    
+    }
+}
